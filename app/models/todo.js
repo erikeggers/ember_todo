8989da27d57fe62a9cf4de72937e4a39d2f1de58
Erik Eggers <em.eggers@gmail.com>
2015-02-26 11:32:53 -0500

    updated

--- app/models/todo.js
+++ app/models/todo.js
@@ -7,33 +7,33 @@ var todos = DS.Model.extend({
 
 
 todos.reopenClass ({
-FIXTURES: [
- {
-   id: 1,
-   title: 'Learn Ember.js',
-   isCompleted: true
- },
- {
-   id: 2,
-   title: 'Actually, Ember CLI',
-   isCompleted: false
- },
- {
-   id: 3,
-   title: 'Profit!',
-   isCompleted: false
- },
- {
-   id: 4,
-   title: 'Eat food',
-   isCompleted: false
- },
- {
-   id: 5,
-   title: 'TIY Front End Class',
-   isCompleted: false
- },
-]
+  FIXTURES: [
+   {
+     id: 1,
+     title: 'Learn Ember.js',
+     isCompleted: true
+   },
+   {
+     id: 2,
+     title: 'Actually, Ember CLI',
+     isCompleted: false
+   },
+   {
+     id: 3,
+     title: 'Profit!',
+     isCompleted: false
+   },
+   {
+     id: 4,
+     title: 'Eat food',
+     isCompleted: false
+   },
+   {
+     id: 5,
+     title: 'TIY Front End Class',
+     isCompleted: false
+   },
+  ]
 });
 
 export default todos;
