0009aafb42439d711530f232474140ce587bc972
Erik Eggers <em.eggers@gmail.com>
2015-02-26 00:24:09 -0500

    finished

---
+++ app/models/todo.js
@@ -0,0 +1,39 @@
+import DS from "ember-data";
+
+var todos = DS.Model.extend({
+  title: DS.attr('string'),
+  isCompleted: DS.attr('boolean')
+});
+
+
+todos.reopenClass ({
+FIXTURES: [
+ {
+   id: 1,
+   title: 'Learn Ember.js',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: 'Actually, Ember CLI',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: false
+ },
+ {
+   id: 4,
+   title: 'Eat food',
+   isCompleted: false
+ },
+ {
+   id: 5,
+   title: 'TIY Front End Class',
+   isCompleted: false
+ },
+]
+});
+
+export default todos;
--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ember-todo',
+    modulePrefix: 'ember_todo',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
